70ef44f3aed233ce544d6fefd40ffd53f50c4b4a
Kelvin Rodriguez <kelv.rod@gmail.com>
2015-01-27 13:05:26 -0400

    Fixed some things and cleaned some files

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,128 @@
-/*$('#header').append();
-$('#main').append('Kelvin')*/
+bio = {
+			"name": "Kelvin Rodriguez",
+			"role": "Mechatronics Technologist",
+			"contacts": {
+				"mobile": "829-556-9057",
+				"email": "kelv.rod@gmail.com",
+				"github": "https://github.com/Kelv",
+				"twitter": "",
+				"location": "Invivienda, Santo Domingo" 
+				},
+			"welcomeMessage": "Hello I'm Kelvin!",
+			"skills": ["Electricity", "Electronics", "Solidworks and AutoCad Design", "Programming"],
+			"biopic": "",
+			"display": "function(){}"
+		};
+education = {
+				"schools": [
+					{
+						"name": "Prof. Simon Orozco Politechnic School",
+						"location": "Invivienda, Santo Domingo, DR",
+						"degree": "Electronics Technician",
+						"majors": [],
+						"dates": "11022010",
+						"url": ""
+					},
+					{
+						"name": "Las Americas Institute of Technology",
+						"location": "Autopista Las Américas, Km. 27, PCSD, La Caleta, Boca Chica, DR",
+						"degree": "Mechatronics Technologist",
+						"majors": [],
+						"dates": "08132014",
+						"url": "http://www.itla.edu.do"
+					},
+					{
+						"name": "UNAPEC",
+						"location": "Avenida 27 de Febrero 569, Santo Domingo",
+						"degree": "Electrical Engineer",
+						"majors": [],
+						"dates": "0",
+						"url": "https://www.unapec.edu.do"
+					}
+				],
+				"onlineCourses": [
+					{
+						"title": "UT.1.01x Energy 101",
+						"school": "UTAustinX",
+						"date": "12042013",
+						"url": "https://s3.amazonaws.com/verify.edx.org/downloads/57a4d25da06f4d0d9a04bbf15a00b11f/Certificate.pdf"
+					},
+					{
+						"title": "6.00.1x Introduction to Computer Science and Programming",
+						"school": "MITx",
+						"date": "01012014",
+						"url": "https://s3.amazonaws.com/verify.edx.org/downloads/387f96a001224d8493bb7aa028d85384/Certificate.pdf"
+					},
+					{
+						"title": "Fundamentals of Audio and Music Engineering: Part 1 Musical Sound & Electronics",
+						"school": "University of Rochester",
+						"date": "07042014",
+						"url": "https://drive.google.com/file/d/0B-7ClsNtH5iac0ttc2paQm91Rnc/view?usp=sharing"
+					},
+					{
+						"title": "Programming Mobile Applications for Android Handheld Systems",
+						"school": "University of Maryland",
+						"date": "04302014",
+						"url": "https://drive.google.com/file/d/0B-7ClsNtH5iaclJMcm9TeHFONFU/view?usp=sharing"
+					},
+					{
+						"title": "UT.6.01x Embedded Systems - Shape the World",
+						"school": "UTAustinX",
+						"date": "05222014",
+						"url": "https://s3.amazonaws.com/verify.edx.org/downloads/bba1866b82ea4a3fa2cac8daa807e8b4/Certificate.pdf"
+					},
+					{
+						"title": "6.00.2x Introduction to Computational Thinking and Data Science",
+						"school": "MITx",
+						"date": "05092014",
+						"url": "https://s3.amazonaws.com/verify.edx.org/downloads/2806ab3e0c2940a9964661a28fd630ed/Certificate.pdf"
+					},
+					{
+						"title": "CNC.ETRRx Introducción al Control Numérico por Computadora",
+						"school": "TenarisUniversityX",
+						"date": "12202014",
+						"url": "https://s3.amazonaws.com/verify.edx.org/downloads/27273b6315c8401bb58fceecf7989c30/Certificate.pdf"
+					},
+					{
+						"title": "Desarrollo en HTML5, CSS y Javascript de WebApps, incl. móviles FirefoxOS* (3.ª ed.)",
+						"school": "Universidad Politecnica de Madrid",
+						"date": "11072014",
+						"url": "https://www.miriadax.net/files/10132/badge/a966af9a-4308-4e3c-8194-643185192329.pdf"
+					}
+				],
+				"display": "function(){}"
+			};
 
-var formattedName = HTMLheaderName.replace("%data%", "Kelvin Rodriguez");
-$('#header').append(formattedName);
+work = {
+			"jobs":[
+			 	{
+					"employer": "Veraguas Water Purifier",
+					"title": "Salesman",
+					"location": "El Progreso Av. #8",
+					"dates": "01102011-07152014",
+					"description": "Sale the product with quality and hygiene."
+				},
+				{
+					"employer": "Misa Electronics",
+					"title": "Electronics Technician",
+					"location": "Simon Orozco Av.",
+					"dates": "08012010-12012010",
+					"description": "Repair electronics equipment."
+				}
+			],
+			"display": "function(){}"
+		};
 
-var formattedRole = HTMLheaderRole.replace("%data%", "Mechatronics Technician");
-$("#header").append(formattedRole);
+projects = {
+				"projects": [
+					{
+						/*"title": ,
+						"dates":,
+						"description":,
+						"images":*/
+					}
+
+				],
+				"display": "function(){}"
+
+			}
